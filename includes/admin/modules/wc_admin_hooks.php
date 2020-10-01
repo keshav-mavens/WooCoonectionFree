4dacec4f4440d4c59bfb7003041560c1b41c8a2d
keshav-mavens <bkeshav@teqmavens.com>
2020-10-01 17:15:18 +0530

    minor changes

--- includes/admin/modules/wc_admin_hooks.php
+++ includes/admin/modules/wc_admin_hooks.php
@@ -16,14 +16,14 @@ function insertProductToApplication( $post_id, $post ){
     		$callback_purpose = 'Insert Woocommerce Product : Process of add woocommerce product to infusionsoft/keap application';
 
     		$wcproductdetails = wc_get_product($post_id);
-			$wcproductPrice = $wcproductdetails->get_regular_price();
-            $wcproductSku = $wcproductdetails->get_sku();
-            if(isset($wcproductSku) && !empty($wcproductSku)){
-            	$wcproductSku = $wcproductSku;
-            }else{
-            	$wcproductSku = "";
-            }
-            $wcproductName = $wcproductdetails->get_name();
+			  $wcproductPrice = $wcproductdetails->get_regular_price();
+        $wcproductSku = $wcproductdetails->get_sku();
+        if(isset($wcproductSku) && !empty($wcproductSku)){
+        	$wcproductSku = $wcproductSku;
+        }else{
+        	$wcproductSku = "";
+        }
+        $wcproductName = $wcproductdetails->get_name();
   			
   			$wcproductDesc = $wcproductdetails->get_description();
   			if(isset($wcproductDesc) && !empty($wcproductDesc)){
@@ -31,8 +31,8 @@ function insertProductToApplication( $post_id, $post ){
   			}else{
   				$wcproductDesc = "";
   			}
-			$wcproductShortDesc = $wcproductdetails->get_short_description();
-			if(isset($wcproductShortDesc) && !empty($wcproductShortDesc)){
+  			$wcproductShortDesc = $wcproductdetails->get_short_description();
+  			if(isset($wcproductShortDesc) && !empty($wcproductShortDesc)){
   				$wcproductShortDesc = $wcproductShortDesc;
   			}else{
   				$wcproductShortDesc = "";
@@ -41,15 +41,15 @@ function insertProductToApplication( $post_id, $post ){
   			$productDetailsArray['active'] = true;
   			$productDetailsArray['product_desc'] = $wcproductDesc;
   			$productDetailsArray['sku'] = $wcproductSku;
-			$productDetailsArray['product_price'] = $wcproductPrice;
+			  $productDetailsArray['product_price'] = $wcproductPrice;
   			$productDetailsArray['product_short_desc'] = $wcproductShortDesc;
   			$productDetailsArray['product_name'] = $wcproductName;
   			$jsonData = json_encode($productDetailsArray);
-			$newProductId = createNewProduct($access_token,$jsonData,$callback_purpose,LOG_TYPE_BACK_END);
-			if(!empty($newProductId)){
-				$callback_purpose = $callback_purpose.' is sucessfully done and newly created product in application is #'.$newProductId;
-				$wooconnection_logs_entry = $wooconnectionLogger->add(LOG_TYPE_BACK_END, $callback_purpose);    
-			}
+  			$newProductId = createNewProduct($access_token,$jsonData,$callback_purpose,LOG_TYPE_BACK_END);
+  			if(!empty($newProductId)){
+  				$callback_purpose = $callback_purpose.' is sucessfully done and newly created product in application is #'.$newProductId;
+  				$wooconnection_logs_entry = $wooconnectionLogger->add(LOG_TYPE_BACK_END, $callback_purpose);    
+  			}
   	}
 
 }
