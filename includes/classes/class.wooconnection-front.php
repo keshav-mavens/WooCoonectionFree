0e484cf557941e2eca26734de69f7d7842dafb92
Ajay Mavens <tajay@teqmavens.com>
2020-11-19 15:22:49 +0530

    lead source code

--- includes/classes/class.wooconnection-front.php
+++ includes/classes/class.wooconnection-front.php
@@ -10,6 +10,7 @@ class WooConnection_Front {
 	public function __construct() {
 		//Call the hook init at the time of plugin initialization..
 		add_action('init', array($this, 'wooconnection_include_frontend_files'));
+		add_action('init', array($this, 'wooconnection_leadsource_handling'));
 	}
 
 
@@ -19,6 +20,62 @@ class WooConnection_Front {
     	require_once(WOOCONNECTION_PLUGIN_DIR . 'includes/modules/generalTriggers/wooconnection-general-register-triggers.php');
     	require_once(WOOCONNECTION_PLUGIN_DIR . 'includes/modules/generalTriggers/wooconnection-general-order-triggers.php');
     }
+
+    //Function Definition : wooconnection_leadsource_handling is used to handle the leadsource funtionality.....
+    public function wooconnection_leadsource_handling(){
+    	//first check lead source id exit in query string.....
+    	if(!empty($_GET['ls'])) {
+			$lsId = $_GET['ls'];//get or set the lead source id....
+			//set cookie....
+			if (!headers_sent()) {
+				$cookieName = "leadsourceId";
+				$cookieValue = $lsId;
+				setcookie($cookieName, $cookieValue, time() + (86400 * 30), "/");
+			}
+		}else{//then check if utm parameters exist in query string...
+			//define empty variables....
+			$lscategory = '';
+			$lsmedium ='';
+			$lsvendor = '';
+			$lsmessage = '';
+
+			//check "utm_source" exist in query string.....
+			if(!empty($_GET['utm_source'])) {
+				$lscategory = $_GET['utm_source'];				
+			}else if (!empty($_COOKIE['lscategory'])) {
+				$lscategory = $_COOKIE['lscategory'];
+			}
+
+			//check "utm_medium" exist in query string.....
+			if(!empty($_GET['utm_medium'])) {
+				$lsmedium = $_GET['utm_medium'];
+			}else if (!empty($_COOKIE['lsmedium'])) {
+				$lsmedium = $_COOKIE['lsmedium'];
+			}
+
+			//check "utm_campaign" exist in query string.....
+			if(!empty($_GET['utm_campaign'])) {
+				$lsvendor = $_GET['utm_campaign'];
+			}else if (!empty($_COOKIE['lsvendor'])) {
+				$lsvendor = $_COOKIE['lsvendor'];
+			}
+
+			//check "utm_content" exist in query string.....
+			if(!empty($_GET['utm_content'])) {
+				$lsmessage = $_GET['utm_content'];
+			}else if (!empty($_COOKIE['lsmessage'])) {
+				$lsmessage = $_COOKIE['lsmessage'];
+			}
+
+			//set utm parameters in cookie.....
+			if (!headers_sent()) {
+				setcookie('lscategory', $lscategory, time() + (86400 * 30), "/"); 
+				setcookie('lsmedium', $lsmedium, time() + (86400 * 30), "/"); 
+				setcookie('lsvendor', $lsvendor, time() + (86400 * 30), "/"); 
+				setcookie('lsmessage', $lsmessage, time() + (86400 * 30), "/"); 
+			}
+		} 
+	}
 }
 	
 // Create global so you can use this variable beyond initial creation.
