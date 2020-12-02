56ec18cf66b6a2eacb2cd993b40de0e3bb2df487
Ajay Mavens <tajay@teqmavens.com>
2020-12-02 17:48:41 +0530

    free version feedback points

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -201,8 +201,18 @@
             //Check if "response" done....
             var checkResponse = getQueryParameter('response');
             if(checkResponse != "" && checkResponse == "1"){
-                swal("Authorization!", 'Application authentication done successfully.', "success");
-                $("#application_settings").after('<span class="custom-icons" onclick="showMarkRelatedData(\'import_products\')"><i class="fa fa-check-circle" aria-hidden="true"></i></span>');
+                $("#application_settings").after('<span class="custom-icons"><i class="fa fa-check-circle" aria-hidden="true"></i></span>');
+                swal({
+                  title: "Authorization!",
+                  text: "Application authentication done successfully.",
+                  type: "success",
+                  confirmButtonText: "OK"
+                },
+                function(isConfirm){
+                  if (isConfirm) {
+                    $('#import_products').trigger('click');
+                  }
+                });
             }
 
             //Match Products Tab : check all products checkbox rule....
@@ -258,12 +268,12 @@
 
             //this code is used to check whether the plugin is activated or not if activated then add "tick" icon in next of activation menu......
             if ($(".activated").length ) {
-                $("#plugin_activation").after('<span class="custom-icons" onclick="showMarkRelatedData(\'application_settings\')"><i class="fa fa-check-circle" aria-hidden="true"></i></span>');
+                $("#plugin_activation").after('<span class="custom-icons"><i class="fa fa-check-circle" aria-hidden="true"></i></span>');
             }
 
             //this code is used to check whether the plugin authentication is done or not if done then add "tick" icon in next of infusionsoft/keap setting menu......
             if ($(".authdone").length ) {
-                $("#application_settings").after('<span class="custom-icons" onclick="showMarkRelatedData(\'import_products\')"><i class="fa fa-check-circle" aria-hidden="true"></i></span>');
+                $("#application_settings").after('<span class="custom-icons"><i class="fa fa-check-circle" aria-hidden="true"></i></span>');
             }
         });
 }(jQuery));
@@ -376,7 +386,7 @@ function activateWcPlugin(){
                     $("#activationKey").val(responsedata.licence_key);
                 }
                 $("#plugin_activation").after('<span class="custom-icons" onclick="showMarkRelatedData(\'application_settings\')"><i class="fa fa-check-circle" aria-hidden="true"></i></span>');
-
+                $('#application_settings').trigger('click');
             }else{
                 $(".activation-details-error").show();
                 $(".common_disable_class").addClass('leftMenusDisable');
@@ -662,34 +672,5 @@ function applyCollapseRules(div_id){
         $('#'+div_id).on('hidden.bs.collapse', function() {
            $("#icon_"+div_id).addClass('fa-caret-down').removeClass('fa-caret-up');
         });    
-    }
-}
-
-//onclick of tick mark from menus....
-function showMarkRelatedData(tab_id){
-    if(tab_id != ""){
-        jQuery(".ajax_loader").show();
-        jQuery(".tab_related_content").addClass('overlay');
-        $("li.sub-menu-expand a").removeClass("active-sub-menu");
-        if(tab_id == 'application_settings'){
-            $("#import_products").removeClass("active");
-            $("#application_settings").addClass("active-sub-menu");
-        }else if(tab_id == 'import_products'){
-            //$("#import_products").addClass("active");
-            $('.accordian-list > li.expanded > a').removeClass('active');
-            $("#import_products").addClass('active');
-            $('.accordian-list > li.expanded > a').next('ul.sub-menu').slideUp();
-            $("#import_products").next('ul.sub-menu').slideToggle();
-        }
-        jQuery.post( ajax_object.ajax_url + "?action=wc_load_tab_main_content",{tab_id:tab_id}, function(data) {
-            jQuery(".ajax_loader").hide();
-            $(".import_tab_content").hide();
-            if(!$('.tab_related_content').is(':visible')){
-               $(".tab_related_content").show(); 
-            }
-            $(".tab_related_content").html('');
-            $(".tab_related_content").html(data);
-            jQuery(".tab_related_content").removeClass('overlay');
-        });
     }
 }
