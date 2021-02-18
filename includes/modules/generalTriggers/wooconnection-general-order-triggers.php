71cdab26537449d091d080e5b943f6549aaacb04
Ajay Mavens <tajay@teqmavens.com>
2021-02-18 09:02:14 +0530

    changesg

--- includes/modules/generalTriggers/wooconnection-general-order-triggers.php
+++ includes/modules/generalTriggers/wooconnection-general-order-triggers.php
@@ -143,6 +143,13 @@ function wooconnection_trigger_status_complete_hook($orderid){
                     //empty the cookie "affiliateId" value...
                     setcookie('affiliateId','',time()-99999,'/',$_SERVER['SERVER_NAME']);
                 }   
+            }else{//empty the affiliate variable and also empty it from the cookie.....
+                $referralAffiliateId = '';
+                //check header is not sent....
+                if(!headers_sent()){
+                    //empty the cookie "affiliateId" value...
+                    setcookie('affiliateId','',time()-99999,'/',$_SERVER['SERVER_NAME']);
+                }
             }
             
             //get order data and update the contact information,,
